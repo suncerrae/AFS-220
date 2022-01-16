0ea79e089233dd86e176e775fa938a824778fa9d
sun <sun.quinones@bryanuniversity.edu>
2022-01-16 17:37:38 -0500

    x

--- week3/Django-App/Scripts/activate.bat
+++ week3/Django-App/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\wmmor\dev\bryan-university\Afs-220-capstone\Django-app
+set VIRTUAL_ENV=C:\Users\scart\dev\bryan-university\afs-220\other\django-app
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(Django-app) %PROMPT%
+set PROMPT=(django-app) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
